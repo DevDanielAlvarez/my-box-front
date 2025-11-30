f56e57f1d25d1f5afc9e3878e6c6e3b5aef83219
Daniel <skyrimdaniel11@gmail.com>
2025-11-30 12:47:03 -0300

    feat: add basic layout to login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,10 @@
 export default function loginPage() {
   return (
-    <div className="bg-red-500">
-      <img src="img/login_image.jpg" />
-      <form></form>
+    <div className="bg-red-400 h-screen grid grid-cols-2">
+      <img className="h-full w-full object-cover" src="img/login_image.jpg" />
+      <div>
+        <form></form>
+      </div>
     </div>
   );
 }
