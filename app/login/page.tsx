bac22a6fc39ceb6c2626a1a379856fc736ed8f3e
Daniel <skyrimdaniel11@gmail.com>
2025-11-30 19:30:21 -0300

    feat: finish html and css login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,29 +1,52 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import Link from "next/link";
 
 export default function loginPage() {
   return (
-    <div className="bg-red-400 h-screen grid grid-cols-2">
-      <img
-        className="h-full w-full overflow-hidden"
-        src="img/login_image.jpg"
-      />
-      {/* items-center(Y-axis) | justify-center(X-axis) */}
-      <div className="flex items-center justify-center">
-        {/* h-120 to send input to top */}
-        <form className="bg-amber-400 w-full h-120 p-12 flex items-center justify-center flex-col">
+    <div className=" h-screen grid grid-cols-2">
+      {/* ==== LEFT COLUMN START ==== */}
+      <img className="h-screen w-full object-cover" src="img/login_image.jpg" />
+      {/* ==== LEFT COLUMN END ==== */}
+
+      {/* ==== RIGHT COLUMN START ==== */}
+      <div className="col-span-1 w-full">
+        {/* LOGO */}
+        <div className="w-full flex justify-center items-center">
+          <img className="w-sm " src={"logo.jpg"} />
+        </div>
+        {/* FORM */}
+        <form className="mt-12 flex gap-2 flex-col">
           {/* EMAIL INPUT */}
-          <div className="grid w-full max-w-sm items-center gap-3">
+          <div className="grid w-full items-center gap-3 pr-24 pl-24">
             <Label htmlFor="email">Email</Label>
-            <Input id="email" type="email" />
+            <Input className="w-full" id="email" type="email" />
           </div>
           {/* PASSWORD INPUT */}
-          <div className="grid w-full max-w-sm items-center gap-3 mt-12">
+          <div className="grid w-full items-center gap-3 pr-24 pl-24">
             <Label htmlFor="password">Password</Label>
             <Input id="password" type="password" />
           </div>
+          <div className="flex justify-end pr-24 mt-1">
+            <Link href={"test"}>
+              <Label className="text-blue-600 cursor-pointer underline">
+                Forgot password?
+              </Label>
+            </Link>
+          </div>
+          <div className="w-full pl-24 pr-24">
+            <Button className=" w-full mt-6">Login</Button>
+          </div>
+          <p className=" flex justify-center gap-1">
+            Do you not have an account?
+            <Link className="text-blue-500" href={"#"}>
+              Sign up
+            </Link>
+          </p>
         </form>
       </div>
+      {/* ==== RIGHT COLUMN END ==== */}
     </div>
   );
 }
