4e3374f80dd22eb660f8d745f85201891e4fa11e
Daniel <skyrimdaniel11@gmail.com>
2025-12-08 16:13:32 -0300

    feat: add toast and states to message

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -6,18 +6,33 @@ import { Eye, EyeOff } from "lucide-react"; // ou ícone equivalente
 import { appRouterContext } from "next/dist/server/route-modules/app-route/shared-modules";
 import Link from "next/link";
 import { useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
 
 export default function registerPage() {
   // ====> START STATES TO FORM FIELDS <====
   // Name
   const [name, setName] = useState("");
+  const [nameFieldMessage, setNameFieldMessage] = useState("");
   // email
   const [email, setEmail] = useState("");
+  const [emailFieldMessage, setEmailFieldMessage] = useState("");
   // password
   const [password, setPassword] = useState("");
+  const [passwordFieldMessage, setPasswordFieldMessage] = useState("");
   // password confirmation
   const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [
+    passwordConfirmationFieldMessage,
+    setPasswordConfirmationFieldMessage,
+  ] = useState("");
   // ====> END STATES TO FORM FIELDS <====
+
+  function resetMessageFields() {
+    setNameFieldMessage("");
+    setEmailFieldMessage("");
+    setPasswordFieldMessage("");
+    setPasswordConfirmationFieldMessage("");
+  }
 
   /**
    * submit form register to back-end serve
@@ -26,6 +41,12 @@ export default function registerPage() {
   async function submitForm(event: React.FormEvent<HTMLFormElement>) {
     //Prevents the default submit logic of html
     event.preventDefault();
+    //reset message fields
+    resetMessageFields();
+    //prevent that form fields are empty
+    if (!formIsValid()) {
+      return;
+    }
     // get api url to concat
     const apiBasePath = process.env.NEXT_PUBLIC_API_URL;
     //await the fetch finish to get response
@@ -42,79 +63,126 @@ export default function registerPage() {
         password_confirmation: passwordConfirmation,
       }),
     });
+
+    if (response.ok) {
+      toast.success("User created successfully");
+      return;
+    }
+    toast.error("Error 🤡");
+  }
+
+  function formIsValid(): boolean {
+    verifyEmptyFields();
+    return (
+      name.trim().length > 0 &&
+      email.trim().length > 0 &&
+      password.trim().length > 0 &&
+      passwordConfirmation.trim().length > 0
+    );
+  }
+  function verifyEmptyFields() {
+    if (name.trim().length == 0) {
+      setNameFieldMessage("The name field not to be empty");
+    }
+    if (email.trim().length == 0) {
+      setEmailFieldMessage("The email field not to be empty");
+    }
+    if (password.trim().length == 0) {
+      setPasswordFieldMessage("The password field not to be empty");
+    }
+    if (passwordConfirmation.trim().length == 0) {
+      setPasswordConfirmationFieldMessage(
+        "The password confirmation field not to be empty"
+      );
+    }
   }
 
   return (
-    <div className=" h-screen grid grid-cols-2">
-      {/* ==== LEFT COLUMN START ==== */}
-      <img className="h-screen w-full object-cover" src="img/login_image.jpg" />
-      {/* ==== LEFT COLUMN END ==== */}
+    <>
+      <ToastContainer />
+      <div className=" h-screen grid grid-cols-2">
+        {/* ==== LEFT COLUMN START ==== */}
+        <img
+          className="h-screen w-full object-cover"
+          src="img/login_image.jpg"
+        />
+        {/* ==== LEFT COLUMN END ==== */}
 
-      {/* ==== RIGHT COLUMN START ==== */}
-      <div className="col-span-1 w-full">
-        {/* LOGO */}
-        <div className="w-full flex justify-center items-center">
-          <img className="w-sm " src={"logo.jpg"} />
+        {/* ==== RIGHT COLUMN START ==== */}
+        <div className="col-span-1 w-full">
+          {/* LOGO */}
+          <div className="w-full flex justify-center items-center">
+            <img className="w-sm " src={"logo.jpg"} />
+          </div>
+          {/* FORM */}
+          <form onSubmit={submitForm} className="mt-12 flex gap-2 flex-col">
+            {/* NAME INPUT */}
+            <div className="grid w-full items-center gap-3 pr-24 pl-24">
+              <Label htmlFor="name">Name</Label>
+              <Input
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+                className="w-full"
+                id="name"
+                type="text"
+              />
+              <span className="text-red-500">{nameFieldMessage}</span>
+            </div>
+            {/* EMAIL INPUT */}
+            <div className="grid w-full items-center gap-3 pr-24 pl-24">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                className="w-full"
+                id="email"
+                type="email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+              <span className="text-red-500">{emailFieldMessage}</span>
+            </div>
+            {/* PASSWORD INPUT */}
+            <div className="grid w-full items-center gap-3 pr-24 pl-24">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+              <span className="text-red-500">{passwordFieldMessage}</span>
+            </div>
+            {/* CONFIRM PASSWORD INPUT*/}
+            <div className="grid w-full items-center gap-3 pr-24 pl-24 relative">
+              <Label htmlFor="confirm password">Confirm your password</Label>
+              <Input
+                id="confirm_password"
+                type="password"
+                value={passwordConfirmation}
+                onChange={(event) =>
+                  setPasswordConfirmation(event.target.value)
+                }
+              />
+
+              <span className="text-red-500">
+                {passwordConfirmationFieldMessage}
+              </span>
+            </div>
+
+            <div className="w-full pl-24 pr-24">
+              <Button className=" w-full mt-6 h-12" type="submit">
+                Register
+              </Button>
+            </div>
+            <p className=" flex justify-center gap-1">
+              Do you have an account?
+              <Link className="text-blue-500" href={"/login"}>
+                Log in
+              </Link>
+            </p>
+          </form>
         </div>
-        {/* FORM */}
-        <form onSubmit={submitForm} className="mt-12 flex gap-2 flex-col">
-          {/* NAME INPUT */}
-          <div className="grid w-full items-center gap-3 pr-24 pl-24">
-            <Label htmlFor="name">Name</Label>
-            <Input
-              value={name}
-              onChange={(event) => setName(event.target.value)}
-              className="w-full"
-              id="name"
-              type="text"
-            />
-          </div>
-          {/* EMAIL INPUT */}
-          <div className="grid w-full items-center gap-3 pr-24 pl-24">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              className="w-full"
-              id="email"
-              type="email"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-            />
-          </div>
-          {/* PASSWORD INPUT */}
-          <div className="grid w-full items-center gap-3 pr-24 pl-24">
-            <Label htmlFor="password">Password</Label>
-            <Input
-              id="password"
-              type="password"
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-            />
-          </div>
-          {/* CONFIRM PASSWORD INPUT*/}
-          <div className="grid w-full items-center gap-3 pr-24 pl-24 relative">
-            <Label htmlFor="confirm password">Confirm your password</Label>
-            <Input
-              id="confirm_password"
-              type="password"
-              value={passwordConfirmation}
-              onChange={(event) => setPasswordConfirmation(event.target.value)}
-            />
-          </div>
-
-          <div className="w-full pl-24 pr-24">
-            <Button className=" w-full mt-6 h-12" type="submit">
-              Register
-            </Button>
-          </div>
-          <p className=" flex justify-center gap-1">
-            Do you have an account?
-            <Link className="text-blue-500" href={"/login"}>
-              Log in
-            </Link>
-          </p>
-        </form>
+        {/* ==== RIGHT COLUMN END ==== */}
       </div>
-      {/* ==== RIGHT COLUMN END ==== */}
-    </div>
+    </>
   );
 }
